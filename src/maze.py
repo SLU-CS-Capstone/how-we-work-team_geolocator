66de7cf68f74337205e9ef31d1233a9d2e5d027e
em-henken <118228404+em-henken@users.noreply.github.com>
2023-09-11 16:52:47 -0500

    Update maze.py

--- src/maze.py
+++ src/maze.py
@@ -1,7 +1,7 @@
 from graph import Graph
 
 class Maze:
-    def __init__(self, size=20):
+    def __init__(self, size):
         self.size = size
         self.nodes = []
         self.graph = Graph(size*size)
